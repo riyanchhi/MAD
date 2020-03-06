3f769df6125c92010ef16a9818c514b0817c176f
riyanchhi <53818109+riyanchhi@users.noreply.github.com>
2020-03-06 11:15:38 +0530

    Create main_activity.java

---
+++ AppIntent/main_activity.java
@@ -0,0 +1,50 @@
+package com.example.labprog_2;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    //Defining the Views
+    EditText e1,e2;
+    Button bt;
+    Spinner s;
+    //Data for populating in Spinner
+    String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
+    String name,reg,dept;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Referring the Views
+        e1= (EditText) findViewById(R.id.editText);
+        e2= (EditText) findViewById(R.id.editText2);
+        bt= (Button) findViewById(R.id.button);
+        s= (Spinner) findViewById(R.id.spinner);
+        //Creating Adapter for Spinner for adapting the data from array to Spinner
+        ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+        s.setAdapter(adapter);
+        //Creating Listener for Button
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name=e1.getText().toString();
+                reg=e2.getText().toString();
+                dept=s.getSelectedItem().toString();
+                //Intent For Navigating to Second Activity
+                Intent i = new Intent(MainActivity.this,Activity2.class);
+                //For Passing the Values to Second Activity
+                i.putExtra("name_key", name);
+                i.putExtra("reg_key",reg);
+                i.putExtra("dept_key", dept);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
