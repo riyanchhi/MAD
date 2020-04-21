f4772bf578331742f20a26d09c598227d5541d85
riyanchhi <53818109+riyanchhi@users.noreply.github.com>
2020-04-21 18:12:39 +0530

    Create MainActivity.java

---
+++ SavedPreferences/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.savedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void loadActivityB(View v){
+        Intent i=new Intent(MainActivity.this, ActivityB.class);
+        startActivity(i);
+    }
+}
