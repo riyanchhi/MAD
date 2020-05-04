0a1899515e78a4416eee54c8573b4631e1d768b1
riyanchhi <53818109+riyanchhi@users.noreply.github.com>
2020-05-04 23:42:14 +0530

    Lab 9

--- NotifyMessage/MainActivity.java
+++ NotifyMessage/MainActivity.java
@@ -11,7 +11,7 @@ import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
+import android.os.Build; 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
